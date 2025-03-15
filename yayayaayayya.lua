fd479c3b68ddd18b40958190734a8cf2f2bd7627
Hexichron <92874538+Royxzr@users.noreply.github.com>
2025-03-15 12:50:29 +0100

    Update yayayaayayya.lua

--- yayayaayayya.lua
+++ yayayaayayya.lua
@@ -1,23 +1,35 @@
 local game_map = {
-	[994732206] = function() -- setclipboard(game.GameId) this how to get game id
-		if _G.KaitunMode then
-			loadstring(game:HttpGet("https://sdhfiudashdfhe12e.xnxx"))()
-		elseif _G.BountyMode then
-			loadstring(game:HttpGet("https://sdhfiudashdfhe12e.xnxx2"))()
-		elseif _G.V4Mode then
-			loadstring(game:HttpGet("https://sdhfiudashdfhe12e.xnxx3"))()
-		else
-			loadstring(game:HttpGet("https://sdhfiudashdfhe12e.xnxx4"))()
-		end
-	end
+    [994732206] = function() -- setclipboard(game.GameId) this how to get game id
+        if _G.KaitunMode then
+            loadstring(game:HttpGet("https://sdhfiudashdfhe12e.xnxx"))()
+        elseif _G.BountyMode then
+            loadstring(game:HttpGet("https://sdhfiudashdfhe12e.xnxx2"))()
+        elseif _G.V4Mode then
+            loadstring(game:HttpGet("https://sdhfiudashdfhe12e.xnxx3"))()
+        else
+            loadstring(game:HttpGet("https://sdhfiudashdfhe12e.xnxx4"))()
+        end
+    end
+
+   [3183403065] = function() -- setclipboard(game.GameId) this how to get game id
+        if _G.VersionControl = "V2" then
+            loadstring(game:HttpGet("https://sdhfiudashdfhe12e.xnxx"))()
+        elseif _G.RoyxSpecialMode = "Hell Factory" then
+            loadstring(game:HttpGet("https://sdhfiudashdfhe12e.xnxx2"))()
+        elseif _G.VersionControl = "Pre-release" then
+            loadstring(game:HttpGet("https://sdhfiudashdfhe12e.xnxx3"))()
+        else
+            loadstring(game:HttpGet("https://sdhfiudashdfhe12e.xnxx4"))()
+        end
+    end
 }
 
 setmetatable(game_map,{
-	__index = function(...)
-		return function()
-			game.Players.LocalPlayer:Kick("Not Found The Gameadpo[sadkpoasdko-paskdop[asd")
-		end
-	end,
+    __index = function(...)
+        return function()
+            game.Players.LocalPlayer:Kick("Not Found The Gameadpo[sadkpoasdko-paskdop[asd")
+        end
+    end,
 })
 
 game_map[game.GameId]()
