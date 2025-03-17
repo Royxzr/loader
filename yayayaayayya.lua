4076eb4ac158b36bfb82bf082555a5bb57c71ff0
KangKung <87695599+KangKung02@users.noreply.github.com>
2025-03-17 15:41:37 +0700

    Update yayayaayayya.lua

--- yayayaayayya.lua
+++ yayayaayayya.lua
@@ -12,11 +12,11 @@ local game_map = {
     end
 
    [3183403065] = function() -- setclipboard(game.GameId) this how to get game id
-        if _G.VersionControl = "V2" then
+        if _G.VersionControl == "V2" then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/royxGameLoader/refs/heads/main/AAOld.lua"))()
-        elseif _G.RoyxSpecialMode = "Hell Factory" then
+        elseif _G.RoyxSpecialMode == "Hell Factory" then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung02/royxGameLoader/refs/heads/main/AAOld.lua"))()
-        elseif _G.VersionControl = "Pre-release" then
+        elseif _G.VersionControl == "Pre-release" then
             loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/vmm3z3d1f9299gyzav9eaiw3lhz5fafd.lua"))()
         else
             loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/vmm3z3d1f9299gyzav9eaiw3lhz5fafd.lua"))()
